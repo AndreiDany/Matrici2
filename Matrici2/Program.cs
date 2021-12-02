21d305e919e7dafdc6f77af7b17cef412b4e2116
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-12-02 19:10:46 +0200

    Matrici2

--- Matrici2/Program.cs
+++ Matrici2/Program.cs
@@ -6,7 +6,37 @@ namespace Matrici2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Console.Write("Introduceti numarul de linii:");
+            int N = Int32.Parse(Console.ReadLine());
+            Console.Write("Introduceti numarul de coloane:");
+            int M = Int32.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[N, M];
+
+            for (int row = 0; row < N; row++)
+                for (int col = 0; col < M; col++)
+                {
+                    Console.Write($"[{row},{col}]");
+                    matrix[row, col] = Int32.Parse(Console.ReadLine());
+                }
+
+            Console.Write("Introduceti linia pe care se va calcula suma:");
+            int R = Int32.Parse(Console.ReadLine());
+            Console.Write("Introduceti coloana pe care se va calcula suma:");
+            int C = Int32.Parse(Console.ReadLine());
+
+            int sumaR = 0;
+            int sumaC = 0;
+
+            for (int i = 0; i < N; i++)
+            { sumaC = sumaC + matrix[i, R]; }
+
+            for (int i = 0; i < M; i++)
+            { sumaR = sumaR + matrix[R, i]; }
+
+            Console.WriteLine($"Suma pe linia {R} este:"+sumaR);
+            Console.WriteLine($"Suma pe coloana {C} este:"+sumaC);
+
         }
     }
 }
